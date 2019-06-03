e4ce8e911577dd522e2e05a23000300b945e483c
shirayner <ray@ray.localdomain>
2019-06-03 23:01:24 +0800

    SpringBoot_11_cd_01_通过gradle+docker部署SpringBoot项目

---
+++ src/main/java/com/ray/study/springbootdockergralde/SpringBootDockerGraldeApplication.java
@@ -0,0 +1,13 @@
+package com.ray.study.springbootdockergralde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerGraldeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerGraldeApplication.class, args);
+	}
+
+}
